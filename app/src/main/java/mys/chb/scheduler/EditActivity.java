3b851debf26763f4d69d05eed86f51070e865a59
chb <chb1828@naver.com>
2020-09-11 14:04:38 +0900

    에디터 레이아웃

--- app/src/main/java/mys/chb/scheduler/EditActivity.java
+++ app/src/main/java/mys/chb/scheduler/EditActivity.java
@@ -2,6 +2,7 @@ package mys.chb.scheduler;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,12 +12,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
+import jp.wasabeef.richeditor.RichEditor;
 import mys.chb.scheduler.db.entity.Diary;
 import mys.chb.scheduler.db.repository.DiaryRepository;
 
 public class EditActivity extends AppCompatActivity {
 
-    TextView content,title;
+    TextView title;
+    RichEditor editor;
     private DiaryRepository diaryRepository;
     private Date currentDate;
 
@@ -26,16 +29,25 @@ public class EditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit);
 
         diaryRepository = new DiaryRepository(getApplicationContext());
-        content=findViewById(R.id.content);
+        editor=findViewById(R.id.editor);
         title=findViewById(R.id.title);
 
+        editor.setEditorHeight(200);
+        editor.setEditorFontSize(22);
+        editor.setEditorFontColor(Color.RED);
+        editor.setEditorBackgroundColor(Color.BLUE);
+        editor.setBackgroundColor(Color.BLUE);
+        editor.setBackgroundResource(R.drawable.bg);
+        editor.setBackground("https://raw.githubusercontent.com/wasabeef/art/master/chip.jpg");
+        editor.setPadding(10, 10, 10, 10);
+        editor.setPlaceholder("오늘의 말을 적어주세요");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Diary newDiary = new Diary();
-        newDiary.setContent(content.getText().toString());
+        newDiary.setContent(editor.getHtml());
         newDiary.setTitle(title.getText().toString());
         newDiary.setCreateDate(currentDate);
         diaryRepository.insert(newDiary);
@@ -53,7 +65,7 @@ public class EditActivity extends AppCompatActivity {
         }
         Diary diary = getContent(currentDate);
         if(diary!=null) {
-            content.setText(diary.getContent());
+            editor.setHtml(diary.getContent());
             title.setText(diary.getTitle());
         }
     }
